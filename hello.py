a939f11b2e17006ac989bce2712023030e4678fc
TheGravyTrain1607 <164960853+TheGravyTrain1607@users.noreply.github.com>
2024-03-25 20:50:50 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+#This program says hello and ask for my name#
+print("Hello World")
+print("What is your name?")
+myname = input()
+print("Nice to meet you,   " + myname)
+print("Hayden is hella cute")
+
